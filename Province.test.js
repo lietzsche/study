ef2b68beac50c2f88af54cb4ce1abb269482129d
bion0625 <stxtory@gmail.com>
2025-10-29 20:31:02 +0900

    리팩터링, 4.4 테스트 추가하기

--- Province.test.js
+++ Province.test.js
@@ -1,11 +1,17 @@
-import { describe, it } from 'node:test';
+import {describe, it, beforeEach} from 'node:test';
 import assert from 'node:assert/strict';
-import { Province } from "./Province.js";
-import { sampleProvinceData } from "./Main.js";
+import {Province} from "./Province.js";
+import {sampleProvinceData} from "./Main.js";
 
 describe('province', () => {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  })
   it('shortfall', () => {
-    const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
   });
+  it('profit', function () {
+    assert.equal(asia.profit, 230);
+  })
 });
