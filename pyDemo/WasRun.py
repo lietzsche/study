0750c1c43af018e6b5b2853506eb90ac22950e2f
bion0625 <stxtory@gmail.com>
2025-11-02 21:39:00 +0900

    테스트 주도 개발(TDD): 20장. 뒷정리하기

--- pyDemo/WasRun.py
+++ pyDemo/WasRun.py
@@ -5,7 +5,8 @@ class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
+    def tearDown(self):
+        self.log += "tearDown "
